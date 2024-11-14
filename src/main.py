205bf7d69767934076b700773d82c530499c3ff7
Kelthon <kelthonbalbino@gmail.com>
2024-11-14 15:29:37 -0300

    feat: fix params, fixed seamtresses avaliable time

--- src/main.py
+++ src/main.py
@@ -1,44 +1,40 @@
 from typing import Dict, List
 
-def seamstress_work_distribution(
+def distribute_seamstress_work(
     seamstresses: List[Dict[str, int]],
     piece_parts: Dict[str, int],
     avaliable_time: int
 ) -> List[Dict[str, int]]:
-    distribution = [{part: 0 for part in piece_parts} for st in seamstresses]
+    distribution = [{part: 0 for part in piece_parts} for _ in seamstresses]
+    seamstress_avaliable_times = [avaliable_time for _ in seamstresses]
 
     for part_name, part_number in piece_parts.items():
-        allocation = 0
-        
-        seamstresses.sort(key= lambda seamstress: seamstress[part_name])
+        remaining_parts = part_number
 
-        for seamstress in seamstresses:
-            if part_number <= 0:
+        seamstresses.sort(key=lambda seamstress: seamstress[part_name])
+
+        for i, seamstress in enumerate(seamstresses):
+            if remaining_parts <= 0:
                 break
-            
-            index = seamstresses.index(seamstress)
-            max_parts = (avaliable_time - allocation) // seamstress[part_name]
-            allocation = min(max_parts, part_number)
-            distribution[index][part_name] += allocation
-            
-            part_number -= allocation
+
+            if seamstress_avaliable_times[i] <= 0:
+                continue
+
+            max_parts_for_seamstress = (seamstress_avaliable_times[i]) // seamstress[part_name]
+            parts_to_allocate = min(max_parts_for_seamstress, remaining_parts)
+
+            distribution[i][part_name] += parts_to_allocate
+            time_spent = parts_to_allocate * seamstress[part_name]
+            seamstress_avaliable_times[i] -= time_spent
+
+            remaining_parts -= parts_to_allocate
     
     return distribution
 
-
 def show_distribution(distribution: List[Dict[str, int]]) -> None:
-    for i in range(len(distribution)):
-        j = 0
-        allocation = ''
-        
-        for part_name, part_number in distribution[i].items():
-            if j != 0:
-                allocation = f'{allocation}, {part_number} {part_name}(s)'
-            else:
-                allocation = f'{part_number} {part_name}(s)'
-                j += 1
-
-        print(f'seamstress {i}: {allocation}')
+    for i, allocation in enumerate(distribution):
+        parts_allocated = ', '.join(f'{number} {part}(s)' for part, number in allocation.items())
+        print(f'seamstress {i + 1}: {parts_allocated}')
 
 if __name__ == '__main__':
     seamstresses = [
@@ -50,11 +46,11 @@ if __name__ == '__main__':
     ]
 
     pieces = {
-        'sleeve': 150,
-        'collar': 150,
-        'button': 150,
+        'sleeve': 50,
+        'collar': 50,
+        'button': 50,
     }
 
-    avaliable_time = 180
+    avaliable_time = 275
 
-    show_distribution(seamstress_work_distribution(seamstresses, pieces, avaliable_time))
+    show_distribution(distribute_seamstress_work(seamstresses, pieces, avaliable_time))
